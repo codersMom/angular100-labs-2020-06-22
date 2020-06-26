59e67eae323b6743cdf71e0c72d5b15bf91d2051
codersMom <55408502+codersMom@users.noreply.github.com>
2020-06-26 14:35:52 -0400

    Add files via upload

---
+++ get-angular-repos.bat
@@ -0,0 +1,5 @@
+
+
+git clone https://github.com/JudyLipinski/angular100-demos-2020-06-22
+git clone https://github.com/JudyLipinski/angular100-labs-2020-06-22
+git clone https://github.com/JudyLipinski/angular100-solutions-2020-06-22
---
+++ get-modernweb-code.bat
@@ -0,0 +1,3 @@
+
+
+git clone https://github.com/JudyLipinski/modernweb-2020-06-15
